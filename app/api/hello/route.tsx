9e723985e2f8e9de260cdbbc5e018a760f5621fa
mariskoch <maris@koch-24.de>
2023-11-27 20:37:10 +0100

    further tests

--- app/api/hello/route.tsx
+++ app/api/hello/route.tsx
@@ -3,17 +3,47 @@ import HelloComponent from "@/components/HelloComponent";
 import puppeteer from "puppeteer";
 import fs from 'fs';
 import { Readable, Stream, Transform, Writable, pipeline } from "stream";
+import { PipeableStream } from "react-dom/server";
 
 export async function POST(request: Request) {
     const ReactDOMServer = (await import('react-dom/server')).default;
-    const htmlStream = await ReactDOMServer.renderToPipeableStream(<ExportTable firstName="Johanna" lastName="Scherer" month={11} year={2023}></ExportTable>, {
-        onShellReady: () => {
-            htmlStream.pipe(process.stdout);
-        }
-    });
-    
-    const res = new Response(null, {
+    const htmlStream = ReactDOMServer.renderToPipeableStream(<ExportTable firstName="Johanna" lastName="Scherer" month={11} year={2023}></ExportTable>);
+
+    /*
+    const readableStream = toReadableStream(htmlStream);
+
+    const htmlString = await streamToString(readableStream);
+    */
+
+    const comp = ExportTable({ firstName: "Johanna", lastName: "Scherer", month: 11, year: 2023 });
+
+    console.log(comp);
+
+    const res = new Response(comp?.toString(), {
         status: 200
     });
+
     return res;
 }
+
+/*
+function toReadableStream(pipeableStream: PipeableStream): ReadableStream {
+    const { readable, writable } = new TransformStream();
+    pipeableStream.pipe(writable);
+    return readable;
+}
+
+async function streamToString(stream: ReadableStream): Promise<string> {
+    const decoder = new TextDecoder();
+    const reader = stream.getReader();
+    let result = '';
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+            break;
+        }
+        result += decoder.decode(value);
+    }
+    return result;
+}
+*/
