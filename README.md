c59e217d3515daa9877cc1cb270943c7a3b68c97
mariskoch <maris@koch-24.de>
2023-12-01 10:38:49 +0100

    export display with search params working

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - nextjs app router route-handlers: [https://nextjs.org/docs/app/building-your-application/routing/route-handlers](https://nextjs.org/docs/app/building-your-application/routing/route-handlers)
 - prisma + postgresql: [https://www.prisma.io/docs/concepts/database-connectors/postgresql](https://www.prisma.io/docs/concepts/database-connectors/postgresql)
 - html to pdf packages: [https://apitemplate.io/blog/how-to-convert-html-to-pdf-using-node-js/](https://apitemplate.io/blog/how-to-convert-html-to-pdf-using-node-js/)
+- url and search params in ssr page: [https://nextjs.org/docs/app/api-reference/file-conventions/page](https://nextjs.org/docs/app/api-reference/file-conventions/page)
--- app/export/view/page.tsx
+++ app/export/view/page.tsx
@@ -1,93 +1,111 @@
-'use client';
-
 import ExportTableSchema from '@/utils/ExportTableSchema';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { TimeHoursMinutes } from '@/utils/DateUtils';
+import { TimeHoursMinutes, getFirstDayOfMonth, getNumberOfDaysInMonth, getWeekdayOfDate } from '@/utils/DateUtils';
+import { prisma } from '@/client';
 
-const View = () => {
-    const [content, setContent] = useState<ExportTableSchema[]>();
+const View = async ({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | string[] | undefined }
+}) => {
+    const yearParam = searchParams.year;
+    const year = Array.isArray(yearParam) ? parseInt(yearParam[0], 10) : parseInt(yearParam || '1970', 10);
 
-    const searchParams = useSearchParams();
-    const year = searchParams.get('year') ?? '1970';
-    const month = searchParams.get('month') ?? '0';
-    const body = JSON.stringify({ year, month, });
+    const monthParam = searchParams.month;
+    const month = Array.isArray(monthParam) ? parseInt(monthParam[0], 10) : parseInt(monthParam || '1', 10);
 
     const firstName = 'Johanna';
     const lastName = 'Scherer';
 
-    useEffect(() => {
-        const fetchContent = async () => {
-            const res = await (await fetch(`/api/export?year=${year}&month=${month}`, { method: 'GET' })).json()
-            res.forEach((element: ExportTableSchema) => {
-                element.day = new Date(element.day);
-                element.endTime = new Date(element.endTime);
-                element.pauseDuration = new Date(element.pauseDuration);
-                element.startTime = new Date(element.startTime);
-            });
-            setContent(res);
-        };
-        fetchContent();
-    }, []);
+    const gte = getFirstDayOfMonth(year, month);
+    const lt = getFirstDayOfMonth(year, month + 1);
+
+    const workTimeEntries = await prisma.workTimeEntry.findMany({
+        where: {
+            day: {
+                gte,
+                lt,
+            }
+        },
+        orderBy: {
+            day: "asc"
+        }
+    });
+
+    const daysInMonth = getNumberOfDaysInMonth(year, month);
+    const tableData: ExportTableSchema[] = [];
+    for (let day = 1; day <= daysInMonth; day++) {
+        const dayDate = new Date();
+        dayDate.setUTCFullYear(year);
+        dayDate.setUTCMonth(month - 1);
+        dayDate.setUTCDate(day);
+        const weekday = getWeekdayOfDate(year, month, day);
+        const startTime = new Date(2023, 11, day, 8, 0);
+        const endTime = new Date(2023, 11, day, 18, 15);
+        const pauseDuration = new Date(2023, 11, day, 0, 30);
+        tableData.push({
+            day: dayDate,
+            weekday,
+            startTime,
+            endTime,
+            pauseDuration
+        });
+    }
 
     return (
-        <div className='flex items-center justify-center'>
-            {content === undefined ? (
-                <div>Loading...</div>
-            ) : (
-                <div className="bg-white w-[794px] h-[1123px] p-0">
-                    <div className="grid grid-cols-2">
-                        <div>
-                            First Name:
-                        </div>
-                        <div>
-                            {firstName}
-                        </div>
-                        <div>
-                            Last Name:
-                        </div>
-                        <div>
-                            {lastName}
-                        </div>
-                        <div>
-                            Year:
-                        </div>
-                        <div>
-                            {year}
-                        </div>
-                        <div>
-                            Month:
-                        </div>
-                        <div>
-                            {month}
-                        </div>
+        <div className='flex justify-center items-center h-screen'>
+            <div className="bg-white w-[794px] h-[1048px] p-0">
+                <div className="grid grid-cols-2">
+                    <div>
+                        First Name:
                     </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>Weekday</th>
-                                <th>Start Time</th>
-                                <th>End Time</th>
-                                <th>Pause Duration</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {content.map((rowData, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{rowData.day.getDate()}</td>
-                                        <td>{rowData.weekday}</td>
-                                        <td>{TimeHoursMinutes(rowData.startTime)}</td>
-                                        <td>{TimeHoursMinutes(rowData.endTime)}</td>
-                                        <td>{TimeHoursMinutes(rowData.pauseDuration)}</td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
+                    <div>
+                        {firstName}
+                    </div>
+                    <div>
+                        Last Name:
+                    </div>
+                    <div>
+                        {lastName}
+                    </div>
+                    <div>
+                        Year:
+                    </div>
+                    <div>
+                        {year}
+                    </div>
+                    <div>
+                        Month:
+                    </div>
+                    <div>
+                        {month}
+                    </div>
                 </div>
-            )}
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Weekday</th>
+                            <th>Start Time</th>
+                            <th>End Time</th>
+                            <th>Pause Duration</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableData.map((rowData, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{rowData.day.getDate()}</td>
+                                    <td>{rowData.weekday}</td>
+                                    <td>{TimeHoursMinutes(rowData.startTime)}</td>
+                                    <td>{TimeHoursMinutes(rowData.endTime)}</td>
+                                    <td>{TimeHoursMinutes(rowData.pauseDuration)}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
