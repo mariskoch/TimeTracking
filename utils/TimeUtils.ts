daab70072d98d00e36c0dfffa3741a683dc96e83
mariskoch <maris@koch-24.de>
2023-12-09 21:44:47 +0100

    fixed wrong times

--- utils/TimeUtils.ts
+++ utils/TimeUtils.ts
@@ -1,7 +1,6 @@
 export function formatTime(value: string): string {
     const length = value.length;
     let formattedValue = '';
-
     if (length === 1) {
         formattedValue = `0${value}:00`;
     } else if (length === 2) {
@@ -13,9 +12,19 @@ export function formatTime(value: string): string {
             formattedValue = `00:00`;
         }
     } else if (length === 3) {
-        formattedValue = `0${value[0]}:${value[1]}${value[2]}`;
+        const lastTwoDigits = parseInt(value.slice(1));
+        if (lastTwoDigits <= 59) {
+            formattedValue = `0${value[0]}:${value[1]}${value[2]}`;
+        } else {
+            formattedValue = `0${value[0]}:00`;
+        }
     } else if (length === 4) {
-        formattedValue = `${value[0]}${value[1]}:${value[2]}${value[3]}`;
+        const lastTwoDigits = parseInt(value.slice(2));
+        if (lastTwoDigits <= 59) {
+            formattedValue = `${value[0]}${value[1]}:${value[2]}${value[3]}`;
+        } else {
+            formattedValue = `${value[0]}${value[1]}:00`;
+        }
     }
 
     return formattedValue;
