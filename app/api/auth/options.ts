36ca4f4bcf33bc082356bf4631b842ade0cad057
mariskoch <maris@koch-24.de>
2023-12-14 23:54:13 +0100

    setup nextauth

---
+++ app/api/auth/options.ts
@@ -0,0 +1,21 @@
+import {NextAuthOptions} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {label: "Username", type: "text", placeholder: "john_doe"},
+                password: {label: "Password", type: "password"},
+            },
+            async authorize(credentials) {
+                return {
+                    id: "1",
+                    name: "Test User",
+                    email: "test@test.de",
+                }
+            }
+        })
+    ]
+}
---
+++ app/api/auth/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from 'next-auth';
+import {options} from "@/app/api/auth/options";
+
+/*
+declare module "next-auth" {
+    interface Session {
+
+    }
+
+    interface User {
+        id: string;
+        name: string;
+        email: string;
+        address: string;
+    }
+
+    interface Account {
+
+    }
+}
+*/
+
+const handler = NextAuth(options);
+
+export {handler as GET, handler as POST};
