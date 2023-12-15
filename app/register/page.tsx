fc8a5e519f0f2eea0b6165af92f5b9b874e46abf
mariskoch <maris@koch-24.de>
2023-12-15 19:17:11 +0100

    setup for registering

---
+++ app/register/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const exportPage = () => {
+    return (
+        <div className='flex flex-col items-center h-screen'>
+            <div className='w-full sm:w-96 px-6 sm:px-0'>
+                <div className='flex items-center justify-center text-4xl mt-12 mb-3 w-full'>
+                    Register
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default exportPage;
