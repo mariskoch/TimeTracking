2788cb38c90c83d4f5f712746a97b415badff065
mariskoch <maris@koch-24.de>
2024-01-11 19:08:53 +0100

    start page button got spinners

--- app/(forms)/page.tsx
+++ app/(forms)/page.tsx
@@ -3,14 +3,22 @@
 import CustomInput from '@/components/CustomInput';
 import Feedback, {FeedbackProps} from '@/components/Feedback';
 import Link from 'next/link';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {getDateAsString, transformDataFormat} from "@/utils/DateUtils";
 import {formatTime} from "@/utils/TimeUtils";
 import {useSession} from "next-auth/react";
+import {BarLoader} from "react-spinners";
 
 export default function Home() {
     const [feedback, setFeedback] = useState<FeedbackProps>();
+    const [isSubmitLoading, setSubmitLoading] = useState<boolean>(false);
+    const [isExportLoading, setExportLoading] = useState<boolean>(false);
     const session = useSession();
+
+    useEffect(() => {
+        setSubmitLoading(false);
+        setExportLoading(false);
+    }, [])
 
     function setError(): void {
         setFeedback({
@@ -21,6 +29,7 @@ export default function Home() {
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        setSubmitLoading(true);
         const data = Object.fromEntries(new FormData(event.target as HTMLFormElement));
 
         data['Date'] = transformDataFormat(data['Date'] as string);
@@ -39,12 +48,15 @@ export default function Home() {
                         message: 'Time submitted successfully.'
                     });
                     (event.target as HTMLFormElement).reset();
+                    setSubmitLoading(false);
                 } else {
                     setError()
+                    setSubmitLoading(false);
                 }
             })
             .catch(error => {
                 setError()
+                setSubmitLoading(false);
             });
     }
 
@@ -79,8 +91,19 @@ export default function Home() {
                                      onBlur={handleBlur} inputMode={"numeric"}></CustomInput>
                         <button type='submit'
                                 className={`bg-blue-600 text-white w-full mt-6 py-2 rounded-md disabled:bg-gray-500`}
-                                disabled={session.status !== 'authenticated'}>
-                            {session.status === 'authenticated' ? 'Submit' : 'Login to track you time'}
+                                disabled={session.status !== 'authenticated' || isSubmitLoading}>
+                            {isSubmitLoading ? (
+                                <BarLoader
+                                    color="rgba(200, 200, 200, 0.3)"
+                                    height={5}
+                                    loading={true}
+                                    speedMultiplier={2}
+                                    width={75}
+                                    className={'mb-[3px]'}
+                                />
+                            ) : (
+                                session.status === 'authenticated' ? 'Submit' : 'Login to track you time'
+                            )}
                         </button>
                     </form>
                 </div>
@@ -89,9 +112,22 @@ export default function Home() {
                 )}
                 {session.status === 'authenticated' && (
                     <div className='w-full flex flex-col items-center justify-center'>
-                        <Link href='/export'
-                              className='bg-blue-900 text-white rounded-md w-full mt-5 py-2 flex justify-center'>
-                            Go to Exports
+                        <Link href='/export' onClick={() => {
+                            setExportLoading(true);
+                        }}
+                              className='bg-blue-900 text-white rounded-md w-full mt-5 py-2 flex justify-center h-[40px] disabled:bg-gray-500'>
+                            {isExportLoading ? (
+                                <BarLoader
+                                    color="rgba(200, 200, 200, 0.3)"
+                                    height={5}
+                                    loading={true}
+                                    speedMultiplier={2}
+                                    width={75}
+                                    className={'mt-[10px]'}
+                                />
+                            ) : (
+                                'Go to Exports'
+                            )}
                         </Link>
                     </div>
                 )}
