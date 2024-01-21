8128c2a7b4d6b60876842e44ce3926213763f4f3
mariskoch <maris@koch-24.de>
2024-01-21 17:33:58 +0100

    changed handling of background color

--- app/(forms)/layout.tsx
+++ app/(forms)/layout.tsx
@@ -3,9 +3,9 @@ import NavBar from "@/components/NavBar";
 
 export default function FormsLayout({children}: { children: React.ReactNode }) {
     return (
-        <>
+        <div className={'bg-[#F1EFEF]'}>
             <NavBar/>
             {children}
-        </>
+        </div>
     );
 }
